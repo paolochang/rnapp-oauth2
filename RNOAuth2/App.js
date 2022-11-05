b9ef3ab29776d0ecd1cee4b926691a57960e2ac5
Paolo Chang <chanhwan.chang@gmail.com>
2022-11-04 23:39:12 -0400

    delete rnoauth2

--- RNOAuth2/App.js
+++
@@ -1,9 +0,0 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import AppNav from './src/navigation/AppNav';
-
-const App = () => {
-  return <AppNav />;
-};
-
-export default App;
